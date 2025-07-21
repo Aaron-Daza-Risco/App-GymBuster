2f566f02f05706236b4d507e13b6ddb8e8bd33b7
Aaron-Daza-Risco <aarondazarisco@gmail.com>
2025-07-21 16:52:28 -0500

    Crear usuarios con 2 roles empelado/cliente

--- src/main/java/com/version/gymModuloControl/auth/dto/JwtResponse.java
+++ src/main/java/com/version/gymModuloControl/auth/dto/JwtResponse.java
@@ -2,12 +2,10 @@ package com.version.gymModuloControl.auth.dto;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class JwtResponse {
     private Integer id;
@@ -15,5 +13,27 @@ public class JwtResponse {
     private String tipo = "Bearer";
     private String nombreUsuario;
     private List<String> roles;
+    private List<String> todosLosRoles;
+    private boolean tieneMultiplesRoles;
+
+    public JwtResponse(Integer id, String token, String tipo, String nombreUsuario, List<String> roles) {
+        this.id = id;
+        this.token = token;
+        this.tipo = tipo;
+        this.nombreUsuario = nombreUsuario;
+        this.roles = roles;
+        this.todosLosRoles = roles;
+        this.tieneMultiplesRoles = false;
+    }
     
+    public JwtResponse(Integer id, String token, String tipo, String nombreUsuario, 
+                      List<String> roles, List<String> todosLosRoles, boolean tieneMultiplesRoles) {
+        this.id = id;
+        this.token = token;
+        this.tipo = tipo;
+        this.nombreUsuario = nombreUsuario;
+        this.roles = roles;
+        this.todosLosRoles = todosLosRoles;
+        this.tieneMultiplesRoles = tieneMultiplesRoles;
+    }
 }
